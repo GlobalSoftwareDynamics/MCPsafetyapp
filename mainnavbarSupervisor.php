23b288e80454cc3a1206a8a92399a2c390ccb1df
IgnacioRondon <ignacio.rondon@gsdynamics.com>
2017-05-30 18:06:50 -0500

    Add files via upload

--- mainnavbarSupervisor.php
+++ mainnavbarSupervisor.php
@@ -9,7 +9,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a href="mainSupervisor.php"><img src="image/Moly-Cop.jpg" height="60"></a>
+                <a href="mainSupervisor.php"><img src="image/Logo.png" height="50"></a>
             </div>
             <div class="navbar-collapse collapse">
                 <ul class="nav navbar-nav">
